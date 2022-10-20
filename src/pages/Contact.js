b3fa5efae7fe782e8e1578125cebb9934b1e87f5
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 19:55:48 -0400

    restructured App.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,39 @@
 import React from 'react'
 import { validateEmail } from '../utils/helper';
 
+function ContactForm() {
+  const [formState, setFormState] = useState({name: '', email: '', message: ''});
+  const [errorMessage, setErrorMessage] = useState('');
+  const {name, email, message} = formState;
+
+  function handleChange(e) {
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage('Must use a valid Email address');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is Required.`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value})
+    }
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+}
+
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <>
   )
 }
 
-export default Contact;
+export default ContactForm;
