3868517a8b57b86b0b7d2f0f53c25cf06c29f341
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 22:18:40 -0400

    added prop functions to portfolio js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import ProjectCards from '../../src/pages/Project'
+import projects from '../projects.json'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Portfolio = () => {
+// Sets a props to attach to json and display the projects
+
+function Wrapper(props) {
+  return <div className='wrapper'>{props.chidlren}</div>;
+}
+
+function Portfolio () {
   return (
-    <h1>Portfolio</h1>
+    <section>
+      <div className='project'>
+        <h1 className='title'>Portfolio of Ted Shishkovksiy:</h1>
+        <hr></hr>
+      </div>
+
+      <Wrapper id="card-data">
+      {projects.map((project) => (<ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>))}
+      </Wrapper>
+    </section>
   )
 }
+
 
 export default Portfolio;
