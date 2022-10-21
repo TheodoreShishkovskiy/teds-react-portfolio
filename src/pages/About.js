99fbccb75a7cf74999b9f13441a2c748d80216b5
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 21:12:52 -0400

    added about me with image and text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import Profile from '../assets/images/profile.png';
 
-const About = props => {
-  return (
-    <div className='container'>
-      <h1>About</h1>
-      <p>Hi my name is Ted S</p>
-    </div>
+function About() {
+  return(
+    <section className='title'>
+      <h1 class="name">About: Ted Shishkovksiy</h1>
+      <hr></hr>
+
+      <div className='row justify-content-center'>
+        <div className='col-10' id='about-me'>
+          <img class="mb-5" src={Profile} alt="Ted Shishkovskiy"/>
+          <p>
+            Hi my name is Ted Shishkovksiy!
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
