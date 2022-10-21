f8a6e95a5a61895ae171fd2f128c3dc484a67d56
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 23:18:14 -0400

    added the rest of the project images

--- src/data.js
+++ src/data.js
@@ -1,4 +1,9 @@
 import Image from './assets/images/project.png';
+import Image2 from './assets/images/project2.png';
+import Image3 from './assets/images/project3.png';
+import Image4 from './assets/images/project4.png';
+import Image5 from './assets/images/project5.png';
+import Image6 from './assets/images/project6.png';
 
 export default [
   {
@@ -12,7 +17,7 @@ export default [
   {
   "id": 2,
   "name": "password generator project",
-  "image": Image,
+  "image": Image2,
   "github": "https://github.com/TheodoreShishkovskiy/password-generator",
   "deploy": "https://theodoreshishkovskiy.github.io/password-generator/",
   "topics": "HTML, CSS, Javascript"
@@ -20,7 +25,7 @@ export default [
   {
   "id": 3,
   "name": "code quiz",
-  "image": Image,
+  "image": Image3,
   "github": "https://github.com/TheodoreShishkovskiy/code-quiz",
   "deploy": "https://theodoreshishkovskiy.github.io/code-quiz/",
   "topics": "HTML, CSS, Javascript"
@@ -28,7 +33,7 @@ export default [
   {
   "id": 4,
   "name": "work day schedule",
-  "image": Image,
+  "image": Image4,
   "github": "https://github.com/TheodoreShishkovskiy/work-day-schedule",
   "deploy": "https://theodoreshishkovskiy.github.io/work-day-schedule/",
   "topics": "HTML, Javascript, Bootstrap, CSS, jQuery, Moment"
@@ -36,7 +41,7 @@ export default [
   {
   "id": 5,
   "name": "tech blog",
-  "image": Image,
+  "image": Image5,
   "github": "https://github.com/TheodoreShishkovskiy/tech-blog",
   "deploy": "https://tech-blog54321.herokuapp.com/",
   "topics": "model-view-controller, bulma, bcrypt, express-js, mysql, sequelize, node-js, nodemon, dotenv, handlebars-js, jest"
@@ -44,7 +49,7 @@ export default [
   {
   "id": 6,
   "name": "note taker",
-  "image": Image,
+  "image": Image6,
   "github": "https://github.com/TheodoreShishkovskiy/note-taker",
   "deploy": "https://theodore-note-taker.herokuapp.com/",
   "topics": "HTML, CSS, Javascript, Express.js, Node.js, Heroku, Jest, JSON, Insomnia"
