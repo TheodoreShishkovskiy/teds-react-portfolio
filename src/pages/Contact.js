3e93a129d313bf26b5c9c70169046dd9c1bd525a
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 21:26:08 -0400

    added contact textbox entry to Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { validateEmail } from '../utils/helper';
 
+
+// function to have contact be properly filled out
 function ContactForm() {
   const [formState, setFormState] = useState({name: '', email: '', message: ''});
   const [errorMessage, setErrorMessage] = useState('');
@@ -28,10 +30,41 @@ function ContactForm() {
   function handleSubmit(e) {
     e.preventDefault();
   }
-}
 
-const Contact = () => {
- 
+// contact textbox to contact me
+return (
+  <section class="justify-content-center" id="contact-me">
+    <h1 className='contact'>Contact Me Below!</h1>
+    <hr></hr>
+    <form class="justify-content-center" id="contact-info">
+
+      <div>
+        <label htmlFor='name'>Enter Your Name:</label>
+        <input class="form-control" type="text" name="name" defaultValue={name} onBlur={handleChange}/>
+      </div>
+
+      <div>
+        <label htmlFor='email'>Enter Your Email:</label>
+        <input class="form-control" type="text" name="email" defaultValue={email} onBlur={handleChange}/>
+      </div>
+
+      <div>
+        <label htmlFor='name'>Reason for Contact:</label>
+        <textarea class="form-control" type="text" name="message" defaultValue={message} onBlur={handleChange} rows="5"/>
+      </div>
+
+      {errorMessage && (
+        <div>
+          <p className='error-text'>{errorMessage}</p>
+        </div>
+      )}
+
+      <div>
+        <button data-testid='button' class="btn btn-outline-dark mt-4" type="submit" onSubmit={handleSubmit}>Deliver Message</button>
+      </div>
+    </form>
+  </section>
+);
 }
 
 export default ContactForm;
