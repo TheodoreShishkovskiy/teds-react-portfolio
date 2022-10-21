49d63585cdd398bf04b66b06361a968d5b0f3b8f
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-21 09:26:58 -0400

    Updates

---
+++ precache-manifest.354dcfcd0099dfe1805286329f2062d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e93a842c13522dcfe8612623e52a392c",
+    "url": "/teds-react-portfolio/index.html"
+  },
+  {
+    "revision": "310d07f613e28d37b02b",
+    "url": "/teds-react-portfolio/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "19121774fefe65992f39",
+    "url": "/teds-react-portfolio/static/css/main.14b3c130.chunk.css"
+  },
+  {
+    "revision": "310d07f613e28d37b02b",
+    "url": "/teds-react-portfolio/static/js/2.d460bd61.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/teds-react-portfolio/static/js/2.d460bd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19121774fefe65992f39",
+    "url": "/teds-react-portfolio/static/js/main.569a7fbe.chunk.js"
+  },
+  {
+    "revision": "ae3be0fb718ea41bc2dc",
+    "url": "/teds-react-portfolio/static/js/runtime-main.171db329.js"
+  },
+  {
+    "revision": "49f19d20fad7c7213da6e2b8712af3f6",
+    "url": "/teds-react-portfolio/static/media/profile.49f19d20.png"
+  },
+  {
+    "revision": "cf980269a0b349f5e256c2b0df219a5d",
+    "url": "/teds-react-portfolio/static/media/project.cf980269.png"
+  }
+]);
