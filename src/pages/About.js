171559a09335d1a909a0cc4b98bae12eb726aee6
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 23:50:17 -0400

    final touches before README

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,6 +12,8 @@ function About() {
           <img class="mb-5" src={Profile} alt="Ted Shishkovskiy"/>
           <p>
             Hi my name is Ted Shishkovksiy!
+            I am currently finishing up the UNCC Full Stack Coding Bootcamp.
+            This is just the start of a journey as a future software developer.
           </p>
         </div>
       </div>
