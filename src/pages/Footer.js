a64eaf3a82955cdfa639cd7d22580394aa0891d5
Ted Shishkovskiy <tmshishkovskiy@gmail.com>
2022-10-20 21:06:06 -0400

    added Footer info with insta and resume

---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Footer() {
+return (
+  <div class="footer" id="footer">
+    <a href="https://github.com/TheodoreShishkovskiy"><img src="https://img.icons8.com/color/48/000000/github-2.png" alt="Github" class="icon"/></a>
+    <a href="https://instagram.com/Ted_M_S"><img src="https://img.icons8.com/3d-fluency/100/000000/instagram-new.png" alt="Instagram" class="icon"/></a>
+    <a href="https://instagram.com/Ted_M_S"><img src="https://img.icons8.com/plasticine/100/000000/info.png" alt="Resume" class="icon"/></a>
+  </div>
+)
+}
+
+// <a target="_blank" href="https://icons8.com/icon/nj0Uj45LGUYh/instagram">Instagram icon by Icons8</a>
+// <a target="_blank" href="https://icons8.com/icon/67563/info">Info icon by Icons8</a>
+
+export default Footer;
